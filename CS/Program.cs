6a597dd5798f540414b0d0d3a215a943571d6077
Anthony Sameishchev <anton.sameishchev@devexpress.com>
2025-02-26 15:54:18 +0100

    migrate to .net8

--- CS/Program.cs
+++ CS/Program.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using DevExpress.Office.Services;
+using DevExpress.Security;
+using DevExpress.XtraRichEdit;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 using System.Windows.Forms;
 
 namespace RichIUriStreamProviderExample {
@@ -7,11 +14,47 @@ namespace RichIUriStreamProviderExample {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [STAThread]
-        static void Main() {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+        private static string basePath = Directory.GetCurrentDirectory() + @"\TestDocs\";
+        static void Main(string[] args) {
+            using (RichEditDocumentServer wordProcessor = new RichEditDocumentServer())
+            {
+                // Custom IUriStreamProvider registration
+                IUriStreamService uriStreamService = wordProcessor.GetService<IUriStreamService>();
+                uriStreamService.RegisterProvider(new CustomUriStreamProvider(basePath, "bmp"));
+                wordProcessor.LoadDocument(basePath + "test.html");
+                wordProcessor.SaveDocument("Result.docx", DocumentFormat.OpenXml);
+            }
+            //Open the result
+            var p = new Process();
+            p.StartInfo = new ProcessStartInfo(@"Result.docx")
+            {
+                UseShellExecute = true
+            };
+            p.Start();
+        }
+    }
+    public class CustomUriStreamProvider : IUriStreamProvider
+    {
+        private string basePath;
+        private string imageExtension;
+        public string BasePath { get { return basePath; } set { basePath = value; } }
+        public string ImageExtension { get { return imageExtension; } set { imageExtension = value; } }
+
+        public CustomUriStreamProvider(string basePath, string imageExtension)
+        {
+            BasePath = basePath;
+            ImageExtension = imageExtension;
+        }
+
+        public Stream GetStream(string url)
+        {
+            string fileName = string.Format("{0}.{1}", url.Replace("cid:", string.Empty), ImageExtension);
+            MemoryStream memoryStream = new MemoryStream();
+            using (Image image = Image.FromFile(BasePath + fileName))
+                image.Save(memoryStream, ImageFormat.Bmp);
+            memoryStream.Seek(0, SeekOrigin.Begin);
+
+            return memoryStream;
         }
     }
 }
--- VB/Program.vb
+++ VB/Program.vb
@@ -1,21 +1,76 @@
-﻿Imports System
+﻿Imports DevExpress.Office.Services
+Imports DevExpress.Security
+Imports DevExpress.XtraRichEdit
+Imports System
 Imports System.Collections.Generic
+Imports System.Diagnostics
+Imports System.Drawing
+Imports System.Drawing.Imaging
+Imports System.IO
 Imports System.Windows.Forms
 
 Namespace RichIUriStreamProviderExample
-    Friend NotInheritable Class Program
-
-        Private Sub New()
-        End Sub
-
+    Module Program
         ''' <summary>
         ''' The main entry point for the application.
         ''' </summary>
-        <STAThread> _
-        Shared Sub Main()
-            Application.EnableVisualStyles()
-            Application.SetCompatibleTextRenderingDefault(False)
-            Application.Run(New Form1())
+        Private basePath As String = Directory.GetCurrentDirectory() & "\TestDocs\"
+
+        Sub Main(args As String())
+            Using wordProcessor As New RichEditDocumentServer()
+                ' Custom IUriStreamProvider registration
+                Dim uriStreamService As IUriStreamService = wordProcessor.GetService(Of IUriStreamService)()
+                uriStreamService.RegisterProvider(New CustomUriStreamProvider(basePath, "bmp"))
+                wordProcessor.LoadDocument(basePath & "test.html")
+                wordProcessor.SaveDocument("Result.docx", DocumentFormat.OpenXml)
+            End Using
+
+            ' Open the result
+            Dim p As New Process()
+            p.StartInfo = New ProcessStartInfo("Result.docx") With {
+                .UseShellExecute = True
+            }
+            p.Start()
         End Sub
+    End Module
+
+    Public Class CustomUriStreamProvider
+        Implements IUriStreamProvider
+
+        Private _basePath As String
+        Private _imageExtension As String
+
+        Public Property BasePath As String
+            Get
+                Return _basePath
+            End Get
+            Set(value As String)
+                _basePath = value
+            End Set
+        End Property
+
+        Public Property ImageExtension As String
+            Get
+                Return _imageExtension
+            End Get
+            Set(value As String)
+                _imageExtension = value
+            End Set
+        End Property
+
+        Public Sub New(basePath As String, imageExtension As String)
+            Me.BasePath = basePath
+            Me.ImageExtension = imageExtension
+        End Sub
+
+        Public Function GetStream(url As String) As Stream Implements IUriStreamProvider.GetStream
+            Dim fileName As String = String.Format("{0}.{1}", url.Replace("cid:", String.Empty), ImageExtension)
+            Dim memoryStream As New MemoryStream()
+            Using image As Image = Image.FromFile(BasePath & fileName)
+                image.Save(memoryStream, ImageFormat.Bmp)
+            End Using
+            memoryStream.Seek(0, SeekOrigin.Begin)
+            Return memoryStream
+        End Function
     End Class
 End Namespace
